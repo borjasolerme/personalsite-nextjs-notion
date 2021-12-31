4e65bb579ea13086c6bdf3496e133343425d7104
Borja Soler Reyes <borjasolerreyes@MacBook-Pro-de-Borja.local>
2021-12-31 19:03:19 +0100

    new footer design that acs as a navbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaPencilAlt, FaHome, FaLinkedin, FaUserAstronaut, FaBookmark } from 'react-icons/fa'
 import { IoSunnyOutline, IoMoonSharp } from 'react-icons/io5'
 import * as config from 'lib/config'
 
@@ -26,7 +26,43 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Built by {config.author} 🇪🇸</div>
+      <a
+        className={styles.linkedin}
+        href={`https://www.linkedin.com/in/${config.linkedin}`}
+        title={`LinkedIn ${config.author}`}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaHome />
+      </a>
+      <a
+        className={styles.linkedin}
+        href={`https://www.linkedin.com/in/${config.linkedin}`}
+        title={`LinkedIn ${config.author}`}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaPencilAlt />
+      </a>
+      <a
+        className={styles.linkedin}
+        href={`https://www.linkedin.com/in/${config.linkedin}`}
+        title={`LinkedIn ${config.author}`}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaUserAstronaut />
+      </a>
+      <a
+        className={styles.linkedin}
+        href={`https://www.linkedin.com/in/${config.linkedin}`}
+        title={`LinkedIn ${config.author}`}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaBookmark />
+      </a>
+      <hr className="vertical-bar" />
 
       {hasMounted ? (
         <div className={styles.settings}>
@@ -40,43 +76,6 @@ export const Footer: React.FC<{
         </div>
       ) : null}
 
-      <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
-
-        {config.github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${config.github}`}
-            title={`GitHub @${config.github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
-
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
-      </div>
     </footer>
   )
 }
